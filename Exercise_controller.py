b2702c2e3d6ff97664ce4ee216de9597c21c97cc
Liyunfan1998 <16302010002@fudan.edu.cn>
2020-07-15 01:52:48 +0800

    ui embed video player

--- Exercise_controller.py
+++ Exercise_controller.py
@@ -10,9 +10,9 @@ from instructions_modified import *
 # Note That we have to grant access to the camera if we are using a Mac!
 # Either Update your pycharm to 2020.1.3 or use terminal with access settings!
 
-class Instruction_controller(QDialog):
+class Exercise_controller(QDialog):
     def __init__(self, parent=None, rootController=None):
-        super(Instruction_controller, self).__init__(parent)
+        super(Exercise_controller, self).__init__(parent)
         self.rootController = rootController
         self.con = None
         self.ui = Ui_MainWindow_Instructions()
@@ -107,7 +107,6 @@ class VideoController:
 
     def screenShot(self):
         pass
-
 
 
 # This file should not be ran as main entry!
